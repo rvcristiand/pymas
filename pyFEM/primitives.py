d221e4c9a1e4c3cd740d7706ebc61e2118f7b412
rvcristiand <rvcristiand@gmail.com>
2019-01-27 23:13:20 -0500

    truss class refactored

--- pyFEM/primitives.py
+++ pyFEM/primitives.py
@@ -43,46 +43,48 @@ class Node(AttrDisplay):
 
 
 class Truss(AttrDisplay):
-    def __init__(self, parent, label, node_i, node_j, section):
-        self.parent = parent
+    number_nodes = 2
+    number_degrees_freedom_per_node = 3
 
+    def __init__(self, label, node_i, node_j, section):
         self.label = label
         self.node_i = node_i
         self.node_j = node_j
         self.section = section
 
-        self.position = self.node_i.position
-        self.orientation = self._quaternion_from_two_vectors(self.local_vector())
+    def get_local_vector(self):
+        vector = self.node_j.position - self.node_i.position
 
-    def matrix_transformation(self):
-        _t = self.orientation.as_dcm()
-        t = np.zeros(2 * (2 * self.parent.number_degrees_freedom,))
+        return vector / linalg.norm(vector)
 
-        t[0:self.parent.number_degrees_freedom, 0:self.parent.number_degrees_freedom] = _t
-        t[self.parent.number_degrees_freedom:, self.parent.number_degrees_freedom:] = _t
-
-        return t
-
-    def local_vector(self):
-        return self.node_j.position - self.node_i.position
-
-    @staticmethod
-    def _quaternion_from_two_vectors(v_to):
+    def get_orientation(self):
         v_from = np.array([1, 0, 0])
-        v_to = v_to / linalg.norm(v_to)
+        v_to = self.get_local_vector()
 
         if np.all(v_from == v_to):
             return Rotation.from_quat([0, 0, 0, 1])
+
         elif np.all(v_from == -v_to):
             return Rotation.from_quat([0, 0, 1, 0])
+
         else:
             w = np.cross(v_from, v_to)
             w = w / linalg.norm(w)
             theta = np.arccos(np.dot(v_from, v_to))
 
-            return Rotation.from_quat([x * np.sin(theta/2)for x in w] + [np.cos(theta/2)])
+            return Rotation.from_quat([x * np.sin(theta/2) for x in w] + [np.cos(theta/2)])
 
-    def get_k(self):
+    def get_matrix_transformation(self):
+        _t = self.get_orientation().as_dcm()
+        t = np.zeros(2 * (self.number_nodes * self.number_degrees_freedom_per_node,))
+
+        for i in range(self.number_nodes):
+            t[i*self.number_degrees_freedom_per_node:(i + 1) * self.number_degrees_freedom_per_node,
+              i*self.number_degrees_freedom_per_node:(i + 1) * self.number_degrees_freedom_per_node] = _t
+
+        return t
+
+    def get_local_stiff_matrix(self):
         k = self.get_modulus() * self.get_area() / self.get_length()
         k = k * np.array([[1, 0, 0, -1, 0, 0],
                           [0, 0, 0, 0, 0, 0],
@@ -92,7 +94,11 @@ class Truss(AttrDisplay):
                           [0, 0, 0, 0, 0, 0]
                           ])
 
-        t = self.matrix_transformation()
+        return k
+
+    def get_global_stiff_matrix(self):
+        k = self.get_local_stiff_matrix()
+        t = self.get_matrix_transformation()
 
         return np.dot(np.dot(t, k), np.transpose(t))
 
@@ -106,7 +112,7 @@ class Truss(AttrDisplay):
         return distance.euclidean(self.node_i.position, self.node_j.position)
 
     def __eq__(self, other):
-        return self.node_i == other.node_i and self.node_j == other.node_j
+        return self.label == other.label or (self.node_i == other.node_i and self.node_j == other.node_j)
 
 
 class Support(AttrDisplay):
